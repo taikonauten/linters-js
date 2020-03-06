45bfec2e667b231addfb0d1ecb4015836000ccfd
Kevin Woblick <kw@taikonauten.com>
2020-03-06 12:49:34 +0100

    Add the first version of the JavaScript linter package

---
+++ eslint/index.js
@@ -0,0 +1,30 @@
+module.exports = {
+
+  extends: 'eslint:recommended',
+  rules: {
+    'arrow-spacing': [2, {'before': true, 'after': true}],
+    'brace-style': [2, '1tbs', {'allowSingleLine': true}],
+    'comma-dangle': [2, 'only-multiline'],
+    'comma-spacing': [2, {'before': false, 'after': true}],
+    'curly': [2],
+    'eol-last': [2],
+    'func-call-spacing': [2, 'never'],
+    'indent': [2, 2, {'SwitchCase': 1}],
+    'keyword-spacing': [2],
+    'linebreak-style': [2, 'unix'],
+    'no-console': [2, {'allow': ['warn', 'error', 'assert', 'info']}],
+    'no-multiple-empty-lines': [2],
+    'no-param-reassign': [2],
+    'no-unused-vars': [2, {'vars': 'all', 'args': 'none'}],
+    'padding-line-between-statements': [2, { blankLine: 'always', prev: '*', next: 'return' }],
+    'quotes': [2, 'single'],
+    'semi': [2, 'always'],
+    'space-before-blocks': [2, {
+      'functions': 'always',
+      'keywords': 'always',
+      'classes': 'always'
+    }],
+    'space-infix-ops': [2],
+    'space-unary-ops': [2, {'words': true, 'nonwords': false}],
+  }
+};
---
+++ test/wrong.js
@@ -0,0 +1,39 @@
+export default class Taiko{
+  /**
+   * This is a valid comment
+   * @param $el
+   */
+  constructor($el)
+  {
+    this.$el=$el;
+    this.registered = false;
+    this.sum = 1+2;
+    this.quotes = "double";
+
+    this.$element = $el.querySelector('.submit');
+    this.$alert = $el.querySelector('.alert');
+
+    // This is a valid comment
+    this.$element.addEventListener('click', this.onSubmitClick.bind(this));
+  }
+
+  onSubmitClick(event){
+      this.registered = true;
+    console.log('User registered!');
+    this.$alert.innerText = 'The user was successfully registered. A ' +
+      'verification email was sent. Please verify your ';
+  }
+}
+
+const callback = function() {
+  const test = 1;
+  test = 2;
+
+  return `The number is ${test}`;
+};
+
+
+
+callback();
+
+const unused = 'bla';
