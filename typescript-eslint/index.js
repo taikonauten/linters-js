67ac5281fc932a195ad14dfb4a951968328430c4
Tobias Schubert <contact@tobias-schubert.com>
2020-10-15 11:37:52 +0200

    add typescript rules

---
+++ typescript-eslint/index.js
@@ -0,0 +1,18 @@
+module.exports = {
+
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking'
+  ],
+  rules: {
+    '@typescript-eslint/brace-style': [2, '1tbs', {'allowSingleLine': true}],
+    '@typescript-eslint/comma-spacing': [2, {'before': false, 'after': true}],
+    '@typescript-eslint/func-call-spacing': [2, 'never'],
+    '@typescript-eslint/indent': [2, 2, {'SwitchCase': 1}],
+    '@typescript-eslint/keyword-spacing': [2],
+    '@typescript-eslint/no-unused-vars': [2, {'vars': 'all', 'args': 'none'}],
+    '@typescript-eslint/quotes': [2, 'single'],
+    '@typescript-eslint/semi': [2, 'always']
+  }
+};
